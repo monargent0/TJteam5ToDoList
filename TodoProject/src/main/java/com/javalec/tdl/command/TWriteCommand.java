7ee97235594a02459cf58249b7ec53cfdfe9577e
monargent0 <owjddms1118@gmail.com>
2022-03-29 22:01:08 +0900

    session

--- TodoProject/src/main/java/com/javalec/tdl/command/TWriteCommand.java
+++ TodoProject/src/main/java/com/javalec/tdl/command/TWriteCommand.java
@@ -2,19 +2,23 @@ package com.javalec.tdl.command;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.javalec.tdl.dao.TDaoDT;
 
 public class TWriteCommand implements TCommand {
 
 	public void execute (HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
 		
 		String todoContent = request.getParameter("todoContent");
 		String dDay = request.getParameter("dDay");
 		String importance = request.getParameter("importance");
 		String todoStatus = request.getParameter("todoStatus");
+		String userId = (String)session.getAttribute("userId");
 		
 		TDaoDT dao = new TDaoDT();
-		dao.write(todoContent, dDay, importance, todoStatus);
+		dao.write(todoContent, dDay, importance, todoStatus,userId);
+		
 	}
 }
--- TodoProject/src/main/java/com/javalec/tdl/homecontroller/TFrontController.java
+++ TodoProject/src/main/java/com/javalec/tdl/homecontroller/TFrontController.java
@@ -2,7 +2,6 @@ package com.javalec.tdl.homecontroller;
 
 import java.io.IOException;
 
-import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 
 import com.javalec.tdl.command.TCommand;
 import com.javalec.tdl.command.TDeleteCommand;
@@ -50,7 +50,8 @@ public class TFrontController extends HttpServlet {
 	
 	public void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		HttpSession session = request.getSession(true);
+		HttpSession session = request.getSession();
+		
 		
 		String viewPage = null;
 		TCommand command = null;
