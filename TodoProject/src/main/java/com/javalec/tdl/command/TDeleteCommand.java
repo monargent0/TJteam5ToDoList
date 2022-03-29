81c8c89644ea096aa3994bb4b39792028c9282ef
jiheok <100196693+jiheok@users.noreply.github.com>
2022-03-29 16:11:44 +0900

    Update TDeleteCommand.java

--- TodoProject/src/main/java/com/javalec/tdl/command/TDeleteCommand.java
+++ TodoProject/src/main/java/com/javalec/tdl/command/TDeleteCommand.java
@@ -3,16 +3,21 @@ package com.javalec.tdl.command;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
- com.javalec.tdl.dao.TDaoT;
+import com.javalec.tdl.dao.TDaoT;
+
 
 public class TDeleteCommand implements TCommand {
 
-	public void excute(HttpServletRequest request, HttpServletResponse response) {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
 		
 		int listCode = Integer.parseInt(request.getParameter("listCode"));
 		
 		TDaoT daoT = new TDaoT();
 		daoT.delete(listCode);
+		
+		
 	}
 	
 }
