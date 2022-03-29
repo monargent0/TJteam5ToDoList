f671df17dd07585459f6e2127c1c303e823e8cd1
swyoo39 <swyoo39@naver.com>
2022-03-29 19:01:24 +0900

    TListCommand, TWriteCommand

--- TodoProject/src/main/java/com/javalec/tdl/command/TListCommand.java
+++ TodoProject/src/main/java/com/javalec/tdl/command/TListCommand.java
@@ -1,5 +1,21 @@
 package com.javalec.tdl.command;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javalec.tdl.dao.TDaoDT;
+import com.javalec.tdl.dto.TDto;
+
 public class TListCommand implements TCommand {
 
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+		TDaoDT dao = new TDaoDT();
+		ArrayList<TDto> dtos = dao.list();
+		request.setAttribute("list", dtos);
+	}
 }
--- TodoProject/src/main/java/com/javalec/tdl/command/TWriteCommand.java
+++ TodoProject/src/main/java/com/javalec/tdl/command/TWriteCommand.java
@@ -1,5 +1,20 @@
 package com.javalec.tdl.command;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javalec.tdl.dao.TDaoDT;
+
 public class TWriteCommand implements TCommand {
 
+	public void execute (HttpServletRequest request, HttpServletResponse response) {
+		
+		String todoContent = request.getParameter("todoContent");
+		String dDay = request.getParameter("dDay");
+		String importance = request.getParameter("important");
+		String status = request.getParameter("status");
+		
+		TDaoDT dao = new TDaoDT();
+		dao.write(todoContent, dDay, importance, status);
+	}
 }
