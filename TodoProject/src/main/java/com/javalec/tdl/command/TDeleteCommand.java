d5782b6bb9d458b97c97ba8903ca703dba52f773
jiheok <100196693+jiheok@users.noreply.github.com>
2022-03-29 15:49:15 +0900

    TDeleteCommand

--- TodoProject/src/main/java/com/javalec/tdl/command/TDeleteCommand.java
+++ TodoProject/src/main/java/com/javalec/tdl/command/TDeleteCommand.java
@@ -1,5 +1,18 @@
 package com.javalec.tdl.command;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javalec.tdl.dao.TDaoT;
+
 public class TDeleteCommand implements TCommand {
 
+	public void excute(HttpServletRequest request, HttpServletResponse response) {
+		
+		int listCode = Integer.parseInt(request.getParameter("listCode"));
+		
+		TDaoT daoT = new TDaoT();
+		daoT.delete(listCode);
+	}
+	
 }
