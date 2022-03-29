4e55546405b645cb09ea71f6d697f2b33cc85649
monargent0 <owjddms1118@gmail.com>
2022-03-29 14:09:56 +0900

    DaoC,D,T

--- TodoProject/src/main/java/com/javalec/tdl/dao/TDaoD.java
+++ TodoProject/src/main/java/com/javalec/tdl/dao/TDaoD.java
@@ -1,5 +1,19 @@
 package com.javalec.tdl.dao;
 
-public class TDaoD {
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
+public class TDaoD {
+	DataSource dataSource;
+	
+	public TDaoD() {
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/todolist");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	} // Constructor
+	
 }
